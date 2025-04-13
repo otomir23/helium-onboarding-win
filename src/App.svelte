50db6f064c61eab75aa4e41bbf0599db32dcf8fe
wukko <me@wukko.me>
2025-04-13 21:15:20 +0600

    app: barebones welcome page, css, and other sugar

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
+    import "@fontsource-variable/instrument-sans";
     import * as Browser from "./lib/browser";
+
+    import { s } from "./lib/strings";
+
+    import HeliumLogo from "./icons/HeliumLogo.svelte";
+    import IconCheck from "./icons/tabler/IconCheck.svelte";
+    import IconArrowRight from "./icons/tabler/IconArrowRight.svelte";
 
     const prefs = Browser.getPrefs();
     let debug = "";
@@ -18,46 +25,90 @@
             }
         }
     };
+
+    const useDefaults = async () => {
+        await Browser.setPref('services.enabled', true);
+        window.open("chrome://newtab", "_self");
+    }
 </script>
 
 <main>
-    {#await prefs}
-        loading prefs...
-    {:then prefs}
-        {#each Object.entries(prefs) as [pref, val]}
-            <div>
-                {#if typeof val === "boolean"}
-                    <input
-                        type="checkbox"
-                        name={pref}
-                        checked={val}
-                        on:change={handleInput}
-                    />
-                    {pref}
-                {:else if typeof val === "string"}
-                    {pref}
-                    <input
-                        type="text"
-                        name={pref}
-                        value={val}
-                        on:change={handleInput}
-                    />
-                {/if}
+    <div id="welcome-page">
+        <div id="welcome-top">
+            <div id="welcome-logo">
+                <HeliumLogo />
             </div>
-        {/each}
-    {/await}
-    <textarea>{debug}</textarea>
+            <div id="welcome-text">
+                <h1>{s.welcome.greeting}</h1>
+                <p>{s.welcome.body}</p>
+            </div>
+            <div id="welcome-buttons">
+                <button on:click={useDefaults}>
+                    <IconCheck />
+                    {s.button.useDefaults}
+                </button>
+                <button class="primary" tabindex="0">
+                    <IconArrowRight />
+                    {s.button.configure}
+                </button>
+            </div>
+        </div>
+        <div id="welcome-footer">
+            <p>{s.welcome.defaultsNote}</p>
+        </div>
+    </div>
 </main>
 
 <style>
-    textarea {
-        display: block;
-        width: 800px;
-        height: 320px;
-        margin-top: 1em;
+    main {
+        height: 100vh;
     }
 
-    * {
-        padding: 0.5em;
+    #welcome-top,
+    #welcome-text {
+        display: flex;
+        flex-direction: column;
+    }
+
+    #welcome-page {
+        text-align: center;
+        height: 100%;
+        justify-content: center;
+        align-items: center;
+        max-width: 700px;
+    }
+
+    #welcome-top {
+        height: 100%;
+        gap: 32px;
+        justify-content: center;
+        align-items: center;
+    }
+
+    #welcome-logo :global(svg) {
+        height: 64px;
+        width: 64px;
+    }
+
+    #welcome-text {
+        gap: 20px;
+
+        & p {
+            font-size: 20px;
+        }
+    }
+
+    #welcome-buttons {
+        display: flex;
+        gap: var(--gap-2);
+    }
+
+    #welcome-footer {
+        bottom: 32px;
+        position: absolute;
+        display: flex;
+        justify-content: center;
+        width: 100%;
+        left: 0;
     }
 </style>
---
+++ src/lib/strings.ts
@@ -0,0 +1,14 @@
+export const s = {
+    button: {
+        useDefaults: "Use defaults",
+        configure: "Configure",
+        next: "Next",
+        back: "Back",
+        getStarted: "Get started"
+    },
+    welcome: {
+        greeting: "Welcome to the lighter internet",
+        body: "Let's configure your browser just the way you want it.\nIf you're in a rush, you can stick to defaults with best privacy and usability.",
+        defaultsNote: "If you use defaults, Helium online services will be enabled."
+    }
+}
