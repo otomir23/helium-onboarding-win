f7a2361ab33fff11167089107505c41c5f49d733
wukko <me@wukko.me>
2025-04-14 00:40:19 +0600

    app/components: add PageHeader component

---
+++ src/components/PageHeader.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    export let title: string;
+    export let subtitle: string;
+    export let icon: ConstructorOfATypedSvelteComponent;
+</script>
+
+<div class="page-header">
+    <div class="title">
+        <svelte:component this={icon} />
+        <h2>{title}</h2>
+    </div>
+    <p>{subtitle}</p>
+</div>
+
+<style>
+    .page-header {
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+
+        & p {
+            font-size: 18px;
+        }
+
+        & .title {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            gap: 12px;
+
+            & > :global(svg) {
+                height: 28px;
+                width: 28px;
+            }
+        }
+    }
+</style>
