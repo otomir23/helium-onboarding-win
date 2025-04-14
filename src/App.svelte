6bb86d1f6a01e4e6703338fa47c752326e313289
wukko <me@wukko.me>
2025-04-14 12:33:19 +0600

    App: hide secondary pages on start but gracefully

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
     import "@fontsource-variable/instrument-sans";
 
+    import { currentPage } from "./lib/onboarding-flow";
+
     import Welcome from "./pages/Welcome.svelte";
     import PageNavigation from "./components/PageNavigation.svelte";
     import HeliumServices from "./pages/HeliumServices.svelte";
@@ -11,14 +13,31 @@
 <main>
     <Welcome />
 
-    <PageNavigation />
-    <HeliumServices />
-    <SearchEngine />
-    <Finish />
+    <div id="secondary" class:visible={$currentPage !== "Welcome"}>
+        <PageNavigation />
+        <HeliumServices />
+        <SearchEngine />
+        <Finish />
+    </div>
 </main>
 
 <style>
     main {
         height: 100vh;
+    }
+
+    /*
+       hide secondary pages on start
+       & allow them to animate out if user goes back to welcome screen.
+       the "out" animation on either of secondary pages takes 0.2s.
+    */
+    #secondary {
+        opacity: 0;
+        transition: 0s opacity;
+        transition-delay: 0.2s;
+    }
+
+    #secondary.visible {
+        opacity: 1;
     }
 </style>
