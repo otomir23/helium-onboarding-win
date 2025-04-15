32d69b6035c2406fa01e090f00b009bc50765247
wukko <me@wukko.me>
2025-04-16 00:23:09 +0600

    components/SearchEngineItem: fallback engine icon
    if the engine icon is not available, then a default search icon will be shown :3
    
    also added appropriate aria attributes

--- src/components/SearchEngineItem.svelte
+++ src/components/SearchEngineItem.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import { setDefaultEngine } from "../lib/browser";
+    import IconSearch from "../icons/tabler/IconSearch.svelte";
 
     type Props = {
         name: string;
@@ -10,23 +11,31 @@
     };
 
     let { name, desc, iconPath, modelIndex, isDefault }: Props = $props();
+
+    let brokenIcon = $state(false);
 </script>
 
 <button
     class="big"
     class:selected={isDefault}
+    aria-pressed={isDefault}
     onclick={() => {
         setDefaultEngine(modelIndex);
     }}
 >
-    <div>
-        <img
-            class="engine-icon"
-            width="32"
-            height="32"
-            src={iconPath}
-            alt="{name} logo"
-        />
+    <div class="engine-icon-container" aria-hidden="true">
+        {#if !brokenIcon}
+            <img
+                class="engine-icon"
+                width="32"
+                height="32"
+                src={iconPath}
+                alt="{name} logo"
+                onerror={() => (brokenIcon = true)}
+            />
+        {:else}
+            <IconSearch />
+        {/if}
     </div>
     <div class="engine-text">
         <h4>{name}</h4>
@@ -46,6 +55,14 @@
         gap: 2px;
     }
 
+    .engine-icon-container {
+        background: var(--helium-elevated-30);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .engine-icon-container,
     .engine-icon {
         width: 32px;
         height: 32px;
